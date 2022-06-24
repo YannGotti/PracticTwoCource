1e53af05b24faac18e62fbffa62e2f3f8f3062a2
MIRON <1111111111aleksey46@gmail.com>
2022-06-24 10:25:39 +0300

    + projects win forms

---
+++ MironovProgressBar/MainForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace MironovProgressBar
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new MironovProgressBar.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(632, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(290, 12);
+            this.progressBar1.Maximum = 100;
+            this.progressBar1.Minimum = 0;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.ProgressBarColor = System.Drawing.Color.Blue;
+            this.progressBar1.Size = new System.Drawing.Size(200, 426);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Value = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "Миронов ProgressBar";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MironovProgressBar/ProgressBar.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MironovProgressBar
+{
+    public partial class ProgressBar : UserControl
+    {
+        public ProgressBar()
+        {
+            InitializeComponent();
+        }
+
+        int min = 0;// Minimum value for progress range
+        int max = 100;// Maximum value for progress range
+        int val = 0;// Current progress
+        Color BarColor = Color.Blue;// Color of progress meter
+
+        protected override void OnResize(EventArgs e)
+        {
+            // Invalidate the control to get a repaint.
+            this.Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            SolidBrush brush = new SolidBrush(BarColor);
+            float percent = (float)(val - min) / (float)(max - min);
+            Rectangle rect = this.ClientRectangle;
+
+            rect.Width = (int)((float)rect.Width * percent);
+
+            g.FillRectangle(brush, rect);
+
+            Draw3DBorder(g);
+
+            brush.Dispose();
+            g.Dispose();
+        }
+
+        public int Minimum
+        {
+            get
+            {
+                return min;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                if (value > max)
+                {
+                    max = value;
+                }
+
+                min = value;
+
+                if (val < min)
+                {
+                    val = min;
+                }
+
+                Invalidate();
+            }
+        }
+
+        public int Maximum
+        {
+            get
+            {
+                return max;
+            }
+
+            set
+            {
+                if (value < min)
+                {
+                    min = value;
+                }
+
+                max = value;
+
+                if (val > max)
+                {
+                    val = max;
+                }
+
+                this.Invalidate();
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                return val;
+            }
+
+            set
+            {
+                int oldValue = val;
+
+                if (value < min)
+                {
+                    val = min;
+                }
+                else if (value > max)
+                {
+                    val = max;
+                }
+                else
+                {
+                    val = value;
+                }
+
+                float percent;
+
+                Rectangle newValueRect = this.ClientRectangle;
+                Rectangle oldValueRect = this.ClientRectangle;
+
+                percent = (float)(val - min) / (float)(max - min);
+                newValueRect.Width = (int)((float)newValueRect.Width * percent);
+
+                percent = (float)(oldValue - min) / (float)(max - min);
+                oldValueRect.Width = (int)((float)oldValueRect.Width * percent);
+
+                Rectangle updateRect = new Rectangle();
+
+
+                if (newValueRect.Width > oldValueRect.Width)
+                {
+                    updateRect.X = oldValueRect.Size.Width;
+                    updateRect.Width = newValueRect.Width - oldValueRect.Width;
+                }
+                else
+                {
+                    updateRect.X = newValueRect.Size.Width;
+                    updateRect.Width = oldValueRect.Width - newValueRect.Width;
+                }
+
+                updateRect.Height = this.Height;
+                Invalidate(updateRect);
+            }
+        }
+
+        public Color ProgressBarColor
+        {
+            get
+            {
+                return BarColor;
+            }
+
+            set
+            {
+                BarColor = value;
+
+                Invalidate();
+            }
+        }
+
+        private void Draw3DBorder(Graphics g)
+        {
+            int PenWidth = (int)Pens.White.Width;
+
+            g.DrawLine(Pens.DarkGray,
+            new Point(this.ClientRectangle.Left, this.ClientRectangle.Top),
+            new Point(this.ClientRectangle.Width - PenWidth, this.ClientRectangle.Top));
+            g.DrawLine(Pens.DarkGray,
+            new Point(this.ClientRectangle.Left, this.ClientRectangle.Top),
+            new Point(this.ClientRectangle.Left, this.ClientRectangle.Height - PenWidth));
+            g.DrawLine(Pens.White,
+            new Point(this.ClientRectangle.Left, this.ClientRectangle.Height - PenWidth),
+            new Point(this.ClientRectangle.Width - PenWidth, this.ClientRectangle.Height - PenWidth));
+            g.DrawLine(Pens.White,
+            new Point(this.ClientRectangle.Width - PenWidth, this.ClientRectangle.Top),
+            new Point(this.ClientRectangle.Width - PenWidth, this.ClientRectangle.Height - PenWidth));
+        }
+    }
+}
